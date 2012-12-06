aa39af645d406e132d9f2899932d791817c2baa0
Federico Vaga <federico.vaga@gmail.com>
2012-12-06 17:06:37 +0100

    setup: add maintainer and project url
    Signed-off-by: Federico Vaga <federico.vaga@gmail.com>

--- setup.py
+++ setup.py
@@ -3,11 +3,13 @@
 from distutils.core import setup
 
 setup(name='PyZio',
-      version='0.1',
+      version='0.2',
       description='Python Object to handle ZIO devices',
       author='Federico Vaga',
       author_email='federico.vaga@gmail.com',
-      url='',
+      maintainer="Federico Vaga",
+      maintainer_email="federico.vaga@gmail.com",
+      url='https://github.com/FedericoVaga/PyZio',
       packages=['PyZio'],
       license='GPLv2',
      )
