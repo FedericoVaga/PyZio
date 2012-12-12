16158e852e41032839f100ef1cfe7ac288c44296
Federico Vaga <federico.vaga@gmail.com>
2012-12-12 20:34:21 +0100

    zConfig: add the list of standard attributes
    Signed-off-by: Federico Vaga <federico.vaga@gmail.com>

--- PyZio/zConfig.py
+++ PyZio/zConfig.py
@@ -10,3 +10,9 @@ devices_path = zio_bus_path + "devices/"
 triggers = [] # list of available triggers (string)
 buffers = []  # list of available buffers (string)
 devices = []  # list of available devices (zdev object)
+
+# sysfs standard attributes name tuples
+zio_dev_attr_name = ("gain_factor", "offset", "resolution-bits", \
+                     "max-sample-rate", "vref-src")
+zio_buf_attr_name = ("max-buffer-len", "max-buffer-kb")
+zio_trg_attr_name = ("re-enable", "pre-samples", "post-samples")
