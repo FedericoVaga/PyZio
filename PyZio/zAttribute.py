8d6073e119e87c762a94575018d01bdf991f8202
Federico Vaga <federico.vaga@gmail.com>
2012-12-14 15:42:07 +0100

    zAttribute.py: use specific method for R/W permissions
    Signed-off-by: Federico Vaga <federico.vaga@gmail.com>

--- PyZio/zAttribute.py
+++ PyZio/zAttribute.py
@@ -6,7 +6,6 @@
 
 import os
 import sys
-import errno
 
 class zAttribute(object):
     """This class handle a single ZIO attribute. It allow only two operations:
@@ -17,15 +16,18 @@ class zAttribute(object):
         self.name = name
         self.path = path
         self.fullPath = os.path.join(path, name)
-
-        self.readable = True if os.access(self.fullPath, os.R_OK) else False
-        self.writable = True if os.access(self.fullPath, os.W_OK) else False
         #print("Attribute found: " + self.fullPath)
+
+
+    def isReadable(self):
+        return os.access(self.fullPath, os.R_OK)
+
+    def isWritable(self):
+        return os.access(self.fullPath, os.W_OK)
+
 
     def getValue(self):
         """It reads the sysfs file of the attribute and it returns the value"""
-        if not self.readable:
-            return -errno.EPERM
 
         with open(self.fullPath, "r") as f:
             try:
@@ -38,8 +40,6 @@ class zAttribute(object):
 
     def setValue(self, val):
         """It writes the sysfs attribute with val."""
-        if not self.writable:
-            return -errno.EPERM
 
         w = str(val)
         with open(self.fullPath, "w") as f:
