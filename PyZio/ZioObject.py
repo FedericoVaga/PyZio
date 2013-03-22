33a4e981c25ad0175f7b988da449c851d6fb5828
Federico Vaga <federico.vaga@gmail.com>
2013-03-22 14:47:16 +0100

    [feature] ZioObject.py: enable and disable can do the opposite job

--- PyZio/ZioObject.py
+++ PyZio/ZioObject.py
@@ -6,35 +6,53 @@
 import os
 
 class ZioObject:
-    """It handles a generic ZIO object. It is an abstract class"""
+    """
+    It handles a generic ZIO object. It is an abstract class that export
+    generic functions and attributes suitable for every objects.
+    """
 
     def __init__(self, path, name):
         self.name = name
         self.path = path
         self.fullpath = os.path.join(self.path, self.name)
-        self.attribute = {} # Dictionary for boject's attributes
-        self.obj_children = [] # List of children attributes
+        self.attribute = {}     # Dictionary for boject's attributes
+        self.obj_children = []  # List of children attributes
         self.invalid_attrs = ["power", "driver", "subsystem", "uevent"]
 
     def is_valid_sysfs_element(self, name):
-        """It returns if a sysfs name is valid or not"""
+        """
+        It returns if a sysfs name is valid or not
+        """
         return not name in self.invalid_attrs
 
     def get_name(self):
-        """It returns the name of the object"""
+        """
+        It returns the name of the object
+        """
         return self.attribute["name"].get_value()
 
     def is_enable(self):
-        """It returns True if this object is enabled, False otherwise"""
+        """
+        It returns True if this object is enabled, False otherwise
+        """
         en = self.attribute["enable"].get_value()
         return (True if en == "1" else False)
 
-    def enable(self):
-        """It enables the object. It raise IO exception if the user cannot
-        enable the object"""
-        self.attribute["enable"].set_value(1)
 
-    def disable(self):
-        """It disable the object. It raise IO exception if the user cannot
-        enable the object"""
-        self.attribute["enable"].set_value(0)
+    def enable(self, status = True):
+        """
+        It enables the object. It raise IO exception if the user cannot
+        enable the object. You can use this function also to disable an
+        object by setting the optional parameter 'status' to 'False'.
+        """
+        val = 1 if status else 0
+        self.attribute["enable"].set_value(val)
+
+    def disable(self, status = True):
+        """
+        It disable the object. It raise IO exception if the user cannot
+        enable the object. You can use this function also to enable an
+        object by setting the optional parameter 'status' to 'True'.
+        """
+        val = 0 if status else 1
+        self.attribute["enable"].set_value(val)
