cd46ffa48fe840b369aedcd2a27cf2eef87ac9e2
Federico Vaga <federico.vaga@gmail.com>
2013-03-22 15:48:29 +0100

    [doc] ZioAttribute.py: Improve documentation comment

--- PyZio/ZioAttribute.py
+++ PyZio/ZioAttribute.py
@@ -7,8 +7,11 @@ from PyZio.ZioUtil import is_readable, is_writable
 import os
 
 class ZioAttribute:
-    """This class handle a single ZIO attribute. It allow only two operations:
-    read and write."""
+    """
+    This class handle a single ZIO attribute. It allow only two operations:
+    read and write. All methods do not handle exception, so the pass to higher
+    class that must handle errors.
+    """
 
     def __init__(self, path, name):
         self.name = name
@@ -16,20 +19,28 @@ class ZioAttribute:
         self.fullpath = os.path.join(path, name)
 
     def is_readable(self):
-        """It returns if the attribute is readable"""
+        """
+        It returns if the attribute is readable
+        """
         return is_readable(self.fullpath)
 
     def is_writable(self):
-        """It returns if the attributes is writable"""
+        """
+        It returns if the attributes is writable
+        """
         return is_writable(self.fullpath)
 
     def get_value(self):
-        """It reads the sysfs file of the attribute and it returns the value"""
+        """
+        It reads the sysfs file of the attribute and it returns the value
+        """
         with open(self.fullpath, "r") as f:
             return f.read().rstrip("\n\r")
 
     def set_value(self, val):
-        """It writes the sysfs attribute with val."""
+        """
+        It writes the sysfs attribute with val.
+        """
         with open(self.fullpath, "w") as f:
             f.write(str(val))
 
