74b18a2a726090e547b0eacbced6de24c3d513be
Federico Vaga <federico.vaga@gmail.com>
2014-01-29 22:06:49 +0100

    PyZio: fix package export
    Signed-off-by: Federico Vaga <federico.vaga@gmail.com>

--- PyZio/__init__.py
+++ PyZio/__init__.py
@@ -4,5 +4,40 @@
 @license: GPLv2
 """
 
-__all__ = ["zDev", "zCset", "zChan", "zTrig", "zBuf", "zUtil", "zConfig",\
-           "zData", "zCtrl", "zAttribute", "zObject"]
+from .ZioAttribute import ZioAttribute
+from .ZioBuf import ZioBuf
+from .ZioChan import ZioChan
+from .ZioCset import ZioCset
+from .ZioDev import ZioDev
+from .ZioTrig import ZioTrig
+from .ZioInterface import ZioInterface
+from .ZioSocket import ZioSocket
+from .ZioCharDevice import ZioCharDevice
+from .ZioObject import ZioObject
+from .ZioCtrl import ZioCtrl, ZioTimeStamp, ZioAddress, ZioCtrlAttr, ZioTLV
+from .ZioError import ZioError, ZioInvalidControl, ZioMissingAttribute
+from .ZioConfig import zio_bus_path, devices_path, triggers, buffers, devices
+
+__all__ = (
+    "ZioAttribute",
+    "ZioBuf",
+    "ZioChan",
+    "ZioCharDevice",
+    "zio_bus_path",
+    "devices_path",
+    "triggers",
+    "buffers",
+    "devices",
+    "ZioCset",
+    "ZioCtrl",
+    "ZioTimeStamp",
+    "ZioAddress",
+    "ZioCtrlAttr",
+    "ZioTLV",
+    "ZioDev",
+    "ZioError",
+    "ZioInterface",
+    "ZioObject",
+    "ZioSocket",
+    "ZioTrig"
+)
